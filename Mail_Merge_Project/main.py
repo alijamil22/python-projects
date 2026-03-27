f9e7f860d711cf5ee918df891d930fc8bd6222c1
alijamil22 <alijamil2223344@gmail.com>
2026-03-27 05:38:21 +0500

    Updated Mail Merge project

--- Mail_Merge_Project/main.py
+++ Mail_Merge_Project/main.py
@@ -1,10 +1,10 @@
-PLACEHOLDER = "[name]"
-with open("./Input/Names/invited_names.txt") as name_file:
+PLACEHOLDER = "[Ali Jamil]"
+with open("A:/My-work-main/udemy2/Mail Merge Project Start/Mail_Merge_Project/Input/Names/invited_names.txt") as name_file:
         names = name_file.readlines()
-with open("./Input/Letters/starting_letter.txt") as starting_file:
+with open("A:/My-work-main/udemy2/Mail Merge Project Start/Mail_Merge_Project/Input/Letters/starting_letter.txt") as starting_file:
         letter_content = starting_file.read()
         for name in names:
                 stripped_name = name.strip()
                 new_letter = letter_content.replace(PLACEHOLDER,stripped_name)
-                with open(f"./Output/ReadyToSend/Letter_for_{stripped_name}.docx",mode="w") as completed_letter:
+                with open(f"A:/My-work-main/udemy2/Mail Merge Project Start/Mail_Merge_Project/Output/ReadyToSend/Letter_for_{stripped_name}.docx",mode="w") as completed_letter:
                         completed_letter.write(new_letter)
